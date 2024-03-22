831331beb91aad27c073851c9a89333ed98bf06e
Prathviraj <95234838+Prathviraj98@users.noreply.github.com>
2024-03-22 21:19:55 +0530

    Add files via upload

---
+++ home.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import time
+import home, SymBot, finance, editor, about
+def app():
+    
+    st.title("SymBot AI")
+    time.sleep(0.05)
+    st.header("Welcome to SymBot")
+    time.sleep(0.05)
+    st.subheader("A crew of AI agents is here to help you solve problems...")
+    time.sleep(0.05)
+    st.markdown('AI crew is a team of artificial intelligence experts who work together to solve problems and answer queries. They have access to real-time information and data, and they can use this information to help them make decisions and solve problems. AI crew members are also able to learn and adapt, so they can continue to improve their skills over time. AI crew members can be used to solve a wide variety of problems, including: * Answering customer questions * Troubleshooting technical problems * Developing new products and services * Conducting research * Making business decisions AI crew members can also be used to help people learn and grow. They can provide personalized instruction, answer questions, and offer feedback. AI crew is a valuable resource for businesses and individuals alike. They can help people solve problems, learn new things, and make better decisions.')
